e6f0f4586eb4fd39c46ae8129b7e6fdbee7a4acd
Wellington Castro <wcesarc@gmail.com>
2017-07-12 20:53:04 -0300

    db

--- ads/settings.py
+++ ads/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2cabba&v*@@vpev5y5b#r7q2-)@$9&k#8)%olhi_)j94@b-v2&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['45.55.190.255']
 
 # APPEND_SLASH  = True
 # Application definition
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -75,10 +74,21 @@ WSGI_APPLICATION = 'ads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ads_db',
+        'USER': 'root',
+        'PASSWORD': 'ads1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
