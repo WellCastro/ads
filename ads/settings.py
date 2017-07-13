7766569dfc8e8c5683a91d472ea44e54b318e87f
Wellington Castro <wcesarc@gmail.com>
2017-07-12 21:21:36 -0300

    settings debug

--- ads/settings.py
+++ ads/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2cabba&v*@@vpev5y5b#r7q2-)@$9&k#8)%olhi_)j94@b-v2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['45.55.190.255']
 
