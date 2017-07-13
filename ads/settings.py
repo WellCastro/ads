917a2d0038b01535968393c7b35a9334daed6319
Wellington Castro <wcesarc@gmail.com>
2017-07-12 21:56:35 -0300

    ssetings

--- ads/settings.py
+++ ads/settings.py
@@ -75,20 +75,13 @@ WSGI_APPLICATION = 'ads.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ads_db',
         'USER': 'django',
         'PASSWORD': 'django1234',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '3306',
     }
 }
